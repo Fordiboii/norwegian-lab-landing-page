4f0ae7b5bab1dd9f880c4197764d8152960f3c8d
Fredrik Jenssen <fredrik.jenssen@alv.no>
2023-10-16 15:51:35 +0200

    Minor refactorings.

--- src/components/order-form/order-form.tsx
+++ src/components/order-form/order-form.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useContext } from "react";
+import React, { FC, useContext, useState } from "react";
 import './order-form.css';
 import { LANGUAGES } from "../../utils/languages";
 import { LanguageContext } from "../../context/language-context";
@@ -14,103 +14,112 @@ const FORM_INPUT_IDS = Object.freeze({
   COUNTRY: 'COUNTRY'
 });
 
-const handleFormInputValidationError = (e: React.FormEvent<HTMLInputElement>, language: string) => {
-  const target = e.target as HTMLInputElement;
-  
-  // only translate for other languages than English
-  if (language !== LANGUAGES.EN) {
-    if (target.validity.valueMissing) {
-      target.setCustomValidity('Vennligst fyll ut feltet.');
-    } else if (target.validity.badInput) {
-      target.setCustomValidity('Feilmelding.')
-    } else if (target.validity.patternMismatch) {
-      target.setCustomValidity('Feilmelding.')
-    } else if (target.validity.rangeOverflow) {
-      target.setCustomValidity('Feilmelding.')
-    } else if (target.validity.rangeUnderflow) {
-      target.setCustomValidity('Feilmelding.')
-    } else if (target.validity.stepMismatch) {
-      target.setCustomValidity('Feilmelding.')
-    } else if (target.validity.tooLong) {
-      target.setCustomValidity('Feilmelding.')
-    } else if (target.validity.tooShort) {
-      target.setCustomValidity('Feilmelding.')
-    } else if (target.validity.typeMismatch) {
-      target.setCustomValidity('Feilmelding.')
-    } else {
-      // TODO: handle clearing custom error correctly
-      // Reset custom error
-      target.setCustomValidity('');
-    }
-  }
-}
-
 const OrderForm: FC = () => {
   const { language } = useContext(LanguageContext);
+  const [submitStatus, setSubmitStatus] = useState(false);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    setSubmitStatus(true);
+  }
+
+  const handleFormInputValidationError = (e: React.FormEvent<HTMLInputElement>, language: string) => {
+    const target = e.target as HTMLInputElement;
+    
+    // only translate for other languages than English
+    if (language !== LANGUAGES.EN) {
+      if (target.validity.valueMissing) {
+        target.setCustomValidity('Vennligst fyll ut feltet.');
+      } else if (target.validity.badInput) {
+        target.setCustomValidity('Feilmelding.')
+      } else if (target.validity.patternMismatch) {
+        target.setCustomValidity('Feilmelding.')
+      } else if (target.validity.rangeOverflow) {
+        target.setCustomValidity('Feilmelding.')
+      } else if (target.validity.rangeUnderflow) {
+        target.setCustomValidity('Feilmelding.')
+      } else if (target.validity.stepMismatch) {
+        target.setCustomValidity('Feilmelding.')
+      } else if (target.validity.tooLong) {
+        target.setCustomValidity('Feilmelding.')
+      } else if (target.validity.tooShort) {
+        target.setCustomValidity('Feilmelding.')
+      } else if (target.validity.typeMismatch) {
+        target.setCustomValidity('Feilmelding.')
+      } else {
+        // TODO: handle clearing custom error correctly
+        // Reset custom error
+        target.setCustomValidity('');
+      }
+    }
+  }
 
   return (
-  <div className="orderFormContainer">
-    <form className="orderForm">
-      <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+    <div className="orderFormContainer">
+      {submitStatus ? (<h2>{"Takk for din bestilling!"}</h2>) : (
+
+      <form onSubmit={handleSubmit} className="orderForm">
+        <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+          <input
+            style={{ flexGrow: 1 }}
+            type="text"
+            pattern={alphaNumericRegex}
+            id={FORM_INPUT_IDS.FIRST_NAME}
+            required
+            placeholder={"Fornavn"}
+            onInvalid={(e) => handleFormInputValidationError(e, language)}
+          />
+          <input
+            style={{ flexGrow: 1 }}
+            type="text"
+            pattern={alphaNumericRegex}
+            id={FORM_INPUT_IDS.SURNAME}
+            required
+            placeholder={"Etternavn"}
+            onInvalid={(e) => handleFormInputValidationError(e, language)}
+          />
+        </div>
         <input
-          style={{ flexGrow: 1 }}
-          type="text"
-          pattern={alphaNumericRegex}
-          id={FORM_INPUT_IDS.FIRST_NAME}
+          type="email"
+          id={FORM_INPUT_IDS.EMAIL}
           required
-          placeholder={"Fornavn"}
-          onInvalid={(e) => handleFormInputValidationError(e, language)}
-        />
-        <input
-          style={{ flexGrow: 1 }}
-          type="text"
-          pattern={alphaNumericRegex}
-          id={FORM_INPUT_IDS.SURNAME}
-          required
-          placeholder={"Etternavn"}
-          onInvalid={(e) => handleFormInputValidationError(e, language)}
-        />
-      </div>
-      <input
-        type="email"
-        id={FORM_INPUT_IDS.EMAIL}
-        required
-        placeholder={"Epost"}
-        onInvalid={(e) => handleFormInputValidationError(e, language)}
-      />
-      <input
-        type="text"
-        id={FORM_INPUT_IDS.ADDRESS}
-        required
-        placeholder={"Gateadresse"}
-        onInvalid={(e) => handleFormInputValidationError(e, language)}
-      />
-      <div style={{ display: "flex", justifyContent: "space-evenly" }}>
-        <input
-          type="text"
-          id={FORM_INPUT_IDS.CITY}
-          required
-          placeholder={"By"}
+          placeholder={"Epost"}
           onInvalid={(e) => handleFormInputValidationError(e, language)}
         />
         <input
           type="text"
-          id={FORM_INPUT_IDS.POSTAL_CODE}
+          id={FORM_INPUT_IDS.ADDRESS}
           required
-          placeholder={"Postnummer"}
+          placeholder={"Gateadresse"}
           onInvalid={(e) => handleFormInputValidationError(e, language)}
         />
-      </div>
-      <input
-        type="text"
-        id={FORM_INPUT_IDS.COUNTRY}
-        required
-        placeholder={"Land"}
-        onInvalid={(e) => handleFormInputValidationError(e, language)}
-      />
-      <input type="submit"></input>
-    </form>
-  </div>
+        <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+          <input
+            type="text"
+            id={FORM_INPUT_IDS.CITY}
+            required
+            placeholder={"By"}
+            onInvalid={(e) => handleFormInputValidationError(e, language)}
+          />
+          <input
+            type="text"
+            id={FORM_INPUT_IDS.POSTAL_CODE}
+            required
+            placeholder={"Postnummer"}
+            onInvalid={(e) => handleFormInputValidationError(e, language)}
+          />
+        </div>
+        <input
+          type="text"
+          id={FORM_INPUT_IDS.COUNTRY}
+          required
+          placeholder={"Land"}
+          onInvalid={(e) => handleFormInputValidationError(e, language)}
+        />
+        <input type="submit" value={"Bestill"}></input>
+      </form>
+      )}
+    </div>
   )
 };
 
