59e8c9160d04b7d764e8c98e985625c5c9189475
Fredrik Jenssen <fredrik.jenssen@alv.no>
2023-10-17 16:54:39 +0200

    Fix a bunch of styling

--- src/components/above-the-fold/above-the-fold.tsx
+++ src/components/above-the-fold/above-the-fold.tsx
@@ -1,20 +1,26 @@
 import React, { FC } from "react";
 import { useTranslation } from "react-i18next";
+import './above-the-fold.css';
 
 const AboveTheFold: FC = () => {
   const { t } = useTranslation();
 
   return (
-    <>
-      <h1>{t('above_the_fold_header')}</h1>
-      <p>
-        {t('above_the_fold_description')}
-      </p>
-      <a href="/order">
-        <button>{t('above_the_fold_order_now')}</button>
-      </a>
-      <div style={{ paddingBottom: '1em' }}></div>
-    </> 
+    <div className="wrapper">
+      <div className="contentWrapper">
+        <h1>{t('above_the_fold_header')}</h1>
+        <p>
+          {t('above_the_fold_description')}
+        </p>
+        <button onClick={() => {
+          const formEl = document.getElementsByClassName('orderFormContainer')?.[0] as HTMLFormElement | null;
+          formEl?.scrollIntoView({ behavior: 'smooth' });
+        }}>{t('above_the_fold_order_now')}</button>
+      </div>
+      <div className="imageContainer">
+        <img src="https://images.squarespace-cdn.com/content/v1/5d87c3ec1b269753a392c57e/1569182116125-QUCJKKES5KLP2L47XRR2/AdobeStock_190816756.jpeg"></img>
+      </div>
+    </div> 
   );
 }
 
