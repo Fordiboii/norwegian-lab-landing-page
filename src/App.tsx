4cdaf77522bd87cd0dac08db8fb62053e12ab9ab
Fredrik Jenssen <fredrik.jenssen@alv.no>
2023-10-17 17:33:54 +0200

    Remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import { FC } from 'react'
 import './App.css'
 import AboveTheFold from './components/above-the-fold/above-the-fold'
 import Footer from './components/footer/footer'
